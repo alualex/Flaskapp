d44a5e5975d039f7a8eb4962e601e3f77886a413
Alice Alex <alicealex6@gmail.com>
2021-05-19 22:21:44 -0400

    New app file and dependancies

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def hello():
-    return "Hello!"
 
+ITEMS = {"france": "France", "australia":"Australia","kenya":"Kenya","usa":"USA"}
+
+def listToString(s):
+    """
+    Convert a list of items into a string of comma separated items. 
+    """
+    if len(s) == 0:
+        return ""
+    elif len(s) == 1:
+        return s[0]
+    return ", and ".join([", ".join(s[:-1]), s[-1]])
+
+
+
+@app.route('/', methods=['GET','POST'])
 def index():
+    check_list = ""
+    check_items = {}
+    
+    
     if request.method == 'POST':
-        print(request.form.getlist('hello'))
+        check_list = listToString(request.form.getlist('switches'))
+        
+        check_items = {k:v for (k,v) in ITEMS.items() if v in check_list}
 
-    return render_template('index.html')
-
-
+    return render_template('index.html', check_list=check_list, **check_items)
+        
+    
 if __name__ == "__main__":
     app.run(debug=True)
